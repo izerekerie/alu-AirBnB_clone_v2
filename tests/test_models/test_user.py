e165e5c8ce177e66c156abe3112407481d49bc6a
izerekerie <izereuwonkundakelia@gmail.com>
2024-09-28 14:19:49 +0200

    fix tes user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,19 +16,19 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertEqual(type(new.first_name), str)  # Expecting an empty string
 
     def test_last_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertEqual(type(new.last_name), str)  # Expecting an empty string
 
     def test_email(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertEqual(type(new.email), str)  # Expecting an empty string
 
     def test_password(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertEqual(type(new.password), str)  # Expecting an empty string
